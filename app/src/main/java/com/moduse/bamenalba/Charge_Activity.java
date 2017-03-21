5e0eea7c3bf4c6b2ae470185d48ca16d11151713
sejung <sp3227@gmail.com>
2017-03-21 13:45:13 +0900

    start

---
+++ app/src/main/java/com/moduse/bamenalba/Charge_Activity.java
@@ -0,0 +1,13 @@
+package com.moduse.bamenalba;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Charge_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.charge_activity);
+    }
+}
---
+++ app/src/main/java/com/moduse/bamenalba/Chatting_Activity.java
@@ -0,0 +1,16 @@
+package com.moduse.bamenalba;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Chatting_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_chater_my);
+
+
+    }
+}
